2ee908f844aa9bd9b1671874819dce1cbdcff47a
mahyarErshad <mahyar.ershad92@gmail.com>
2022-09-05 00:07:59 +0430

    Added necessary elements

--- components/banner.js
+++ components/banner.js
@@ -1,5 +1,11 @@
 function Banner() {
-  return <div>Banner</div>;
+  return (
+    <div>
+      <h1>Coffee Stores</h1>
+      <p>Discover your local coffee shops</p>
+      <button>View stores nearby</button>
+    </div>
+  );
 }
 
 export default Banner;
