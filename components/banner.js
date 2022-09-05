63fe46b8d82175f0054231a28822be002ec6bc7f
mahyarErshad <mahyar.ershad92@gmail.com>
2022-09-05 20:39:45 +0430

    Button text is a prop

--- components/banner.js
+++ components/banner.js
@@ -1,10 +1,10 @@
 import styles from "../styles/Banner.module.css";
-function Banner() {
+function Banner(props) {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Coffee Stores</h1>
       <p className={styles.subtitle}>Discover your local coffee shops</p>
-      <button className={styles.button}>View stores nearby</button>
+      <button className={styles.button}>{props.buttonText}</button>
     </div>
   );
 }
