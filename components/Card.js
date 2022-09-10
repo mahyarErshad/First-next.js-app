0838fe517d1ad5cee9346cbc55f09f20747f40f1
mahyarErshad <mahyar.ershad92@gmail.com>
2022-09-10 23:53:33 +0430

    Edited Card component

--- components/Card.js
+++ components/Card.js
@@ -1,5 +1,17 @@
-function Card() {
-  return <div>Card</div>;
+import Image from "next/image";
+import Link from "next/link";
+
+function Card(props) {
+  return (
+    <>
+      <Link href={props.link}>
+        <a>
+          <h2>{props.title}</h2>
+          <Image src={props.image} width={260} height={160} />
+        </a>
+      </Link>
+    </>
+  );
 }
 
 export default Card;
