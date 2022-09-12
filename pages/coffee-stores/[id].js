bf3d079a22e8bf853835e27ed4c864b7e0827fa0
mahyarErshad <mahyar.ershad92@gmail.com>
2022-09-12 21:51:37 +0430

    Added getStaticProps & getStaticPaths

--- pages/coffee-stores/[id].js
+++ pages/coffee-stores/[id].js
@@ -1,4 +1,17 @@
 import Link from "next/link";
+import coffeeStores from "../../db/database.json";
+
+export function getStaticProps(staticProps) {
+  const params = staticProps.params;
+  props: {
+    coffeeStore: coffeeStores.find((store) => {
+      return store.id === params.id;
+    });
+  }
+}
+export function getStaticPaths() {
+  path: [{ params: { id: 1 } }, { params: { id: 2 } }];
+}
 
 function coffeeStores() {
   return (
