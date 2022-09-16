c7dcac079ffc627dc7035c0409e0e6964515cb5f
mahyarErshad <mahyar.ershad92@gmail.com>
2022-09-17 00:26:17 +0430

    Do not need props anymore

--- components/banner.js
+++ components/banner.js
@@ -1,5 +1,5 @@
 import styles from "../styles/Banner.module.css";
-function Banner(props) {
+function Banner() {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>
